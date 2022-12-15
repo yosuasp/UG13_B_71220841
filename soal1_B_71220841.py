e0dca8c32c5349bf8375290564898e3594fd9778
yosuasp <117884210+yosuasp@users.noreply.github.com>
2022-12-15 12:57:44 +0700

    Add files via upload

---
+++ soal1_B_71220841.py
@@ -0,0 +1,24 @@
+import turtle
+t= turtle.Turtle()
+#ALPHABET Y
+t.up()
+t.goto (-200,25)
+t.down ()
+t.pensize (10)
+t.left(60)
+t.forward(100)
+t.back(50)
+t.left(60)
+t.forward(50)
+
+# NUMBER 8
+u= turtle.Turtle()
+u.up()
+u.goto (-70,25)
+u.down()
+u.pensize(10)
+
+u.forward(50)
+u.back(50)
+
+
